8c7faa4b8ec3c58b6945b4a32b55ffc64acc4868
Asan96 <1158529652@qq.com>
2019-11-01 18:14:12 +0800

    环境搭建页面

--- car/view/api/py3.py
+++ car/view/api/py3.py
@@ -12,9 +12,71 @@ import socket
 import json
 
 
+# 首页简介
 def py3_home(request):
     return render(request, 'api/py3/py3.html')
 
 
+# 环境搭建
 def py3_setting(request):
     return render(request, 'api/py3/py3_setting.html')
+
+
+# 中文编码
+def py3_cncode(request):
+    return render(request, 'api/py3/py3_cncode.html')
+
+
+# 基础语法
+def py3_grammer(request):
+    return render(request, 'api/py3/py3_grammer.html')
+
+
+# 变量类型
+def py3_variable(request):
+    return render(request, 'api/py3/py3_variable.html')
+
+
+# 数据类型
+def py3_data(request):
+    return render(request, 'api/py3/py3_data.html')
+
+
+# 运算符
+def py3_operator(request):
+    return render(request, 'api/py3/py3_operator.html')
+
+
+# 条件语句
+def py3_condition(request):
+    return render(request, 'api/py3/py3_condition.html')
+
+
+# 循环语句
+def py3_loop(request):
+    return render(request, 'api/py3/py3_loop.html')
+
+
+# 函数
+def py3_function(request):
+    return render(request, 'api/py3/py3_function.html')
+
+
+# 模块
+def py3_module(request):
+    return render(request, 'api/py3/py3_module.html')
+
+
+# 高级特性
+def py3_advanced(request):
+    return render(request, 'api/py3/py3_advanced.html')
+
+
+# 内置函数
+def py3_internal_function(request):
+    return render(request, 'api/py3/py3_internal_function.html')
+
+
+# 异常处理
+def py3_error(request):
+    return render(request, 'api/py3/py3_error.html')
